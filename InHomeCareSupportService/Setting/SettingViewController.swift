40b49b9579839d9db3176714e469a013dfe0bf32
muranakar <p1211065@gmail.com>
2022-06-24 07:46:37 +0900

    共有　version1.1.3

--- InHomeCareSupportService/Setting/SettingViewController.swift
+++ InHomeCareSupportService/Setting/SettingViewController.swift
@@ -32,6 +32,14 @@ class SettingViewController: UIViewController {
         jumpToTwitterInformation()
     }
 
+    @IBAction func shareTwitter(_ sender: Any) {
+        shareOnTwitter()
+    }
+
+    @IBAction func shareLine(_ sender: Any) {
+        shareOnLine()
+    }
+
     private func selectRowFilterServiceTypePickerView() {
         guard let loadedFilterServiceType = filterServiceTypeRepository.load() else { return }
         let filterServiceTypeRow = pickerViewItemsOfFilterServiceType.firstIndex(of: loadedFilterServiceType)
@@ -60,6 +68,59 @@ class SettingViewController: UIViewController {
         // 広告読み込み
         bannerView.load(GADRequest())
     }
+    func shareOnTwitter() {
+            // シェアするテキストを作成
+            let text = "全国のケアマネさんの事業所の検索が可能！"
+        // swiftlint:disable:next line_length
+            let hashTag = "#居宅 #居宅支援 #ケアマネ #ケアマネさん #介護支援専門員 #介護保険 #医療保険 #在宅 #介護 #医療\nhttps://apps.apple.com/jp/app/%E3%82%B1%E3%82%A2%E3%83%9E%E3%83%8Dmap-%E5%85%A8%E5%9B%BD%E3%81%AE%E5%B1%85%E5%AE%85%E6%94%AF%E6%8F%B4%E4%BA%8B%E6%A5%AD%E6%89%80%E3%82%92%E6%A4%9C%E7%B4%A2%E5%8F%AF%E8%83%BD/id1628329901"
+            let completedText = text + "\n" + hashTag
+
+            // 作成したテキストをエンコード
+            let encodedText = completedText.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)
+
+            // エンコードしたテキストをURLに繋げ、URLを開いてツイート画面を表示させる
+            if let encodedText = encodedText,
+               let url = URL(string: "https://twitter.com/intent/tweet?text=\(encodedText)") {
+                UIApplication.shared.open(url)
+            }
+        }
+
+        func shareOnLine() {
+            let urlscheme: String = "https://line.me/R/share?text="
+            // swiftlint:disable:next line_length
+            let message = "全国のケアマネさんの事業所の検索が可能！#居宅 #居宅支援 #ケアマネ #ケアマネさん #介護支援専門員 #介護保険 #医療保険 #在宅 #介護 #医療\nhttps://apps.apple.com/jp/app/%E3%82%B1%E3%82%A2%E3%83%9E%E3%83%8Dmap-%E5%85%A8%E5%9B%BD%E3%81%AE%E5%B1%85%E5%AE%85%E6%94%AF%E6%8F%B4%E4%BA%8B%E6%A5%AD%E6%89%80%E3%82%92%E6%A4%9C%E7%B4%A2%E5%8F%AF%E8%83%BD/id1628329901"
+
+            // line:/msg/text/(メッセージ)
+            let urlstring = urlscheme + "/" + message
+
+            // URLエンコード
+            guard let  encodedURL =
+                    urlstring.addingPercentEncoding(withAllowedCharacters: NSCharacterSet.urlQueryAllowed) else {
+                return
+            }
+
+            // URL作成
+            guard let url = URL(string: encodedURL) else {
+                return
+            }
+
+            if UIApplication.shared.canOpenURL(url) {
+                if #available(iOS 10.0, *) {
+                    UIApplication.shared.open(url, options: [:], completionHandler: { (succes) in
+                        //  LINEアプリ表示成功
+                    })
+                }else{
+                    UIApplication.shared.openURL(url)
+                }
+            }else {
+                // LINEアプリが無い場合
+                let alertController = UIAlertController(title: "エラー",
+                                                        message: "LINEがインストールされていません",
+                                                        preferredStyle: UIAlertController.Style.alert)
+                alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default))
+                present(alertController,animated: true,completion: nil)
+            }
+        }
 }
 
 extension SettingViewController: UIPickerViewDelegate {
